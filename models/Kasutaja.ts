2f4de88a4748880558f0828dccda5f75c5f86ba5
HasieEst <ayrtonm0@gmail.com>
2023-03-14 10:48:44 +0200

    Mudeli koostamine

---
+++ models/Kasutaja.ts
@@ -0,0 +1,30 @@
+// export class Kasutaja {
+//     constructor(
+//         public id: number,
+//         public username: string,
+//         public parool: string,
+//         public eesnimi: string,
+//         public perenimi: string,
+//     ) {}
+// }
+
+
+// export class Kasutaja {
+//     constructor(
+//         private _id: number,
+//         public username: string,
+//         private _parool: string,
+//         public eesnimi: string,
+//         public perenimi: string,
+//     ) {}
+//
+//     get id() {
+//         return this._id;
+//     }
+//     get parool(){
+//         return this._parool
+//     }
+//     set parool(newParool: string){
+//         this._parool = newParool
+//     }
+// }
