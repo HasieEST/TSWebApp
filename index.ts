d6493db8e509a915d51cd5305427baab3b1d9650
HasieEst <ayrtonm0@gmail.com>
2023-04-08 12:02:39 +0300

    API päringud mudeliga + list

--- index.ts
+++ index.ts
@@ -1,9 +1,15 @@
 import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
 import stringsController from "./controllers/strings";
 import productsController from "./controllers/products";
 import productsListController from "./controllers/productlist";
 const app: Express = express();
 
+app.use(cors({
+    origin: ['http://localhost:3006']
+}));
+app.use(bodyParser.json());
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
 });
